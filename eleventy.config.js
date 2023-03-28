3d9c3981ce5fabbcb8d9c68d78447174c2d3de80
magikcypress <cyp@rouquin.me>
2023-03-28 12:50:02 +0200

    Update date FR

--- eleventy.config.js
+++ eleventy.config.js
@@ -65,12 +65,12 @@ module.exports = function(eleventyConfig) {
 	// Filters
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).setLocale('fr').toFormat(format || "dd LLLL yyyy");
 	});
 
 	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).setLocale('fr').toFormat('yyyy-LL-dd');
 	});
 
 	// Get the first `n` elements of a collection.
